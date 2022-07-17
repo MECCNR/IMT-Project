f0efe3f83bea5af6028c464bec3cb46cfdbdba34
MECCNR <e.migalckin@yandex.ru>
2022-07-17 19:12:59 +0900

    v2

--- src/IMTProject.java
+++ src/IMTProject.java
@@ -1,7 +1,7 @@
 public class IMTProject {
     public double calculate(double mass, double height) {
-        double superheight = (height / 100) * (height / 100);
-        double vary = mass / superheight;
+        double superHeight = (height / 100) * (height / 100);
+        double vary = mass / superHeight;
         return vary;
     }
 }
